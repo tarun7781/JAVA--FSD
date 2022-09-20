4f20cd7b55d75f46c8cd58d0ccda3fd65440838a
Tarun Penubarthi <88838671+tarun7781@users.noreply.github.com>
2022-09-20 11:56:28 +0530

    commit

---
+++ Phase 4/Assisted Practice Project/ClassPropertyBind/src/app/product-list/product-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  template: `
+  <app-product-list></app-product-list>
+  `,
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
